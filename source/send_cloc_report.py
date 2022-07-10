71ac85190a4719df6d067834dc11b13473f9d89b
Pranam <68323012+prnam@users.noreply.github.com>
2022-07-10 10:29:33 +0000

    fix code

--- source/send_cloc_report.py
+++ source/send_cloc_report.py
@@ -7,33 +7,33 @@ import shutil
 import subprocess
 import sys
 from tempfile import TemporaryDirectory
-
 import requests
 from shellescape import quote
 
 EMAIL_REGEX = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
 GIT_REGEX = r"((git|ssh|http(s)?)|(git@[\w\.]+))(:(\/\/)?)([\w\.@\:\/\-~]+)(\.git)(\/)?"
-MAILGUN_API = os.environ.get("MAILGUN_API")
-MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY")
-MAILGUN_FROM = os.environ.get("MAILGUN_FROM")
+MAILGUN_API = os.getenv("MAILGUN_API")
+MAILGUN_API_KEY = os.getenv("MAILGUN_API_KEY")
+MAILGUN_FROM = os.getenv("MAILGUN_FROM")
 
 
 def clone_git_repo(repo_url: str, emails: list):
     """Clone git repo"""
     with TemporaryDirectory() as tmp_dir:
         logging.info("Current working directoy: %s", os.getcwd())
-        cwd = os.getcwd()
+        current_working_directory = os.getcwd()
         logging.info("Changing the directory....")
         os.chdir(tmp_dir)
         logging.info("Current working directoy: %s", os.getcwd())
+        temporary_working_directory = os.getcwd()
         clone_command = f"git clone {quote(repo_url)}"
         logging.info("Cloning repo using the command: %s", clone_command)
         os.system(clone_command)
-        pygount_scan(cwd, emails)
+        pygount_scan(current_working_directory, emails, temporary_working_directory)
         logging.info("Deleting the %s directory....", tmp_dir)
 
 
-def pygount_scan(cwd: str, emails: list):
+def pygount_scan(cwd: str, emails: list, temporary_working_directory: str):
     """Scan the repo cloned and write the generate report to a file"""
     repo_name = "".join(os.listdir())
     command = f"pygount --format=summary {quote(repo_name)}"
@@ -41,7 +41,7 @@ def pygount_scan(cwd: str, emails: list):
     save_to_file = f"cloc-report-{repo_name.lower()}.txt"
     with open(save_to_file, "w", encoding="utf-8") as file:
         file.write(result)
-    response = send_email(save_to_file, repo_name, emails)
+    response = send_email(save_to_file, repo_name, emails, temporary_working_directory)
     if response.status_code == 200:
         logging.info("Email sent successfully")
         print("Email sent successfully")
@@ -51,28 +51,58 @@ def pygount_scan(cwd: str, emails: list):
         logging.info("Copying the report to your local persistent storage")
         shutil.copy2(save_to_file, cwd)
         logging.info("Report stored at %s with file name %s", cwd, save_to_file)
+        print(f"Report stored at {cwd} with file name {save_to_file}")
     logging.debug("---- CLOC Report for %s repo ----", repo_name)
-    with open(save_to_file, "r", encoding="utf-8") as file:
-        logging.debug(file.read())
+    logging.debug(read_file(save_to_file, temporary_working_directory))
+    # with open(save_to_file, "r", encoding="utf-8") as file:
+    #     logging.debug(file.read())
 
 
-def send_email(report: str, repo_name: str, to_recipients: list):
+def read_file(filename: str, temporary_working_directory: str):
+    """Read files"""
+    with open(
+        f"{quote(temporary_working_directory)}/{quote(filename)}", "r", encoding="utf-8"
+    ) as file:
+        return file.read()
+
+
+def send_email(
+    report: str, repo_name: str, to_recipients: list, temporary_working_dir: str
+):
     """Send the report to email address"""
     logging.info("Preparing email....")
     print("Preparing email....")
-    with open(report, encoding="utf-8") as file:
-        content = file.read()
+    content = read_file(report, temporary_working_dir)
+    # with open(report, encoding="utf-8") as file:
+    #     content = file.read()
     logging.info("File is been read to email text")
-    return requests.post(
-        MAILGUN_API,
-        auth=("api", MAILGUN_API_KEY),
-        data={
-            "from": MAILGUN_FROM,
-            "to": to_recipients,
-            "subject": f'---- CLOC Report for "{repo_name}" repo ----',
-            "text": {content},
-        },
-    )
+    try:
+        return requests.post(
+            MAILGUN_API,
+            auth=("api", MAILGUN_API_KEY),
+            data={
+                "from": MAILGUN_FROM,
+                "to": to_recipients,
+                "subject": f'---- CLOC Report for "{repo_name}" repo ----',
+                "text": {content},
+            },
+        )
+    except requests.exceptions.HTTPError as http_error:
+        logging.error(http_error)
+        print("HTTP error while connecting to your mail service")
+        sys.exit(1)
+    except requests.exceptions.ConnectionError as connection_error:
+        logging.error(connection_error)
+        print("Connection Error while connecting to your mail service")
+        sys.exit(1)
+    except requests.exceptions.Timeout as timeout_error:
+        logging.error(timeout_error)
+        print("Timeout Error while connecting to your mail service")
+        sys.exit(1)
+    except requests.exceptions.RequestException as request_exception_error:
+        logging.error(request_exception_error)
+        print("Unknown error while connecting to your mail service")
+        sys.exit(1)
 
 
 def argument_parser():
@@ -115,6 +145,11 @@ def input_validation(repo: str, emails: list):
 
 def main():
     """Invoking main method to intitate the program"""
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s: %(message)s",
+        filename="myapp.log",
+        level=logging.INFO,
+    )
     print("Program started....")
     logging.info("Program started....")
     args_parser_result = argument_parser()
